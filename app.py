37252c63a0e4607e9a90d1c71f400712635dd2dd
geistebuzyte <geistebuzyte@gmail.com>
2026-08-28 12:12:02 +0300

    app.py

--- app.py
+++ app.py
@@ -495,18 +495,15 @@ st.subheader(
 
 col1, col2 = st.columns(2)
 
-
 with col1:
-
-st.metric(
+    st.metric(
             "Apytikslis 95 % prognozės intervalas",
             f"{intervalo_min:.1%} – {intervalo_max:.1%}"
         )
 
 
 with col2:
-
-st.metric(
+    st.metric(
             "Galimas lankytojų intervalas",
             f"{lankytojai_min:,} – {lankytojai_max:,}".replace(
                 ",",
